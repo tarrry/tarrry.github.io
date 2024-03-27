0016fd5d2ac86643ef64f779d3effe4805b49252
Tarrry <107268873+tarrry@users.noreply.github.com>
2024-03-27 15:25:07 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var SiteDown = false;
 
 function redirect() {
     if (SiteDown === false) {
-        window.location.href = "https://www.youtube.com";
+        window.location.href = "https://tarrry.github.io/home/welcome.html";
     } else {
         return false;
     }
